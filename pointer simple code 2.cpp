cfb7302ff5d93e3adb9f4ec44891ed4563264fd6
Suliman Sharif <sharifsuliman833@gmail.com>
2025-09-14 12:27:41 +0500

    Add files via upload

---
+++ pointer simple code 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int*p,v;
+	v;
+   	p=&v;
+	*p=42;
+	cout<<"v"<<v<<endl;
+	cout<<"*p"<<*p<<endl;
+	cout<<"p"<< p<<endl;
+	cout<<"&v "<< &v<<endl;
+	
+	
+}
